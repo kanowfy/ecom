d79569b537ff028d0cf95fc741867024a13daafa
kanowfy <nguyendung2002hl@gmail.com>
2024-09-15 20:07:32 +0700

    instrument order service

--- order_service/main.go
+++ order_service/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/kanowfy/ecom/order_service/internal/log"
 	"github.com/kanowfy/ecom/order_service/internal/service"
 	"github.com/kanowfy/ecom/order_service/pb"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 
 	"google.golang.org/grpc"
 )
@@ -33,6 +34,7 @@ func main() {
 	flag.StringVar(&cfg.UpstreamAddr.Shipping, "addr.shipping", cfg.UpstreamAddr.Shipping, "shipping service address")
 	flag.StringVar(&cfg.UpstreamAddr.Payment, "addr.payment", cfg.UpstreamAddr.Payment, "payment service address")
 	flag.StringVar(&cfg.UpstreamAddr.Email, "addr.email", cfg.UpstreamAddr.Email, "email service address")
+	flag.StringVar(&cfg.Otel.GrpcEndpoint, "otel.grpcendpoint", cfg.Otel.GrpcEndpoint, "grpc collector endpoint")
 
 	var level = slog.LevelDebug
 
@@ -46,12 +48,20 @@ func main() {
 
 	flag.Parse()
 
+	logger := log.New(os.Stdout, level, true)
+	ctx := context.Background()
+	tp, err := initTracer(ctx, cfg.Otel.GrpcEndpoint, "order service")
+	if err != nil {
+		logger.Error("failed to initialize tracer", "error", err)
+		os.Exit(1)
+	}
+	defer tp.Shutdown(ctx)
+
 	conns := new(grpcconn.Connection)
 	if err := conns.Map(context.Background(), cfg.UpstreamAddr); err != nil {
 		fmt.Printf("failed to establish upstream connections: %v", err)
 		os.Exit(1)
 	}
-	logger := log.New(os.Stdout, level, true)
 	service := service.New(logger, conns)
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
@@ -60,7 +70,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+	)
 	pb.RegisterOrderServer(s, service)
 	logger.Info(fmt.Sprintf("gRPC server listening on %s:%d", cfg.Server.Host, cfg.Server.Port))
 	if err := s.Serve(lis); err != nil {
