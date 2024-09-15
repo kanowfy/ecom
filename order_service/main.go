d97acd740e8cc0cfff259a515e410c5a38781369
kanowfy <nguyendung2002hl@gmail.com>
2024-09-15 15:01:12 +0700

    refactor order service

--- order_service/main.go
+++ order_service/main.go
@@ -1,52 +1,70 @@
 package main
 
 import (
-	"github.com/kanowfy/ecom/order_service/pb"
-	"log"
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
 	"net"
+	"os"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/kanowfy/ecom/order_service/internal/config"
+	"github.com/kanowfy/ecom/order_service/internal/grpcconn"
+	"github.com/kanowfy/ecom/order_service/internal/log"
+	"github.com/kanowfy/ecom/order_service/internal/service"
+	"github.com/kanowfy/ecom/order_service/pb"
 
 	"google.golang.org/grpc"
 )
 
-var (
-	port = ":3006"
-)
-
-type connection struct {
-	catalogSvcAddr string
-	catalogSvcConn *grpc.ClientConn
-
-	cartSvcAddr string
-	cartSvcConn *grpc.ClientConn
-
-	shippingSvcAddr string
-	shippingSvcConn *grpc.ClientConn
-
-	emailSvcAddr string
-	emailSvcConn *grpc.ClientConn
-
-	paymentSvcAddr string
-	paymentSvcConn *grpc.ClientConn
-}
-
-type server struct {
-	conns connection
-	*pb.UnimplementedOrderServer
-}
-
 func main() {
-	srv := &server{}
-	mapConnections(srv)
+	var cfg config.Config
 
-	lis, err := net.Listen("tcp", port)
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("failed to parse environment variable: %v", err)
+		os.Exit(1)
+	}
+
+	flag.StringVar(&cfg.Server.Host, "srv.host", cfg.Server.Host, "server host")
+	flag.IntVar(&cfg.Server.Port, "srv.port", cfg.Server.Port, "server port")
+	flag.StringVar(&cfg.UpstreamAddr.Catalog, "addr.catalog", cfg.UpstreamAddr.Catalog, "catalog service address")
+	flag.StringVar(&cfg.UpstreamAddr.Cart, "addr.cart", cfg.UpstreamAddr.Cart, "cart service address")
+	flag.StringVar(&cfg.UpstreamAddr.Shipping, "addr.shipping", cfg.UpstreamAddr.Shipping, "shipping service address")
+	flag.StringVar(&cfg.UpstreamAddr.Payment, "addr.payment", cfg.UpstreamAddr.Payment, "payment service address")
+	flag.StringVar(&cfg.UpstreamAddr.Email, "addr.email", cfg.UpstreamAddr.Email, "email service address")
+
+	var level = slog.LevelDebug
+
+	flag.Func("loglevel", "minimum log level", func(s string) error {
+		if err := level.UnmarshalText([]byte(s)); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	flag.Parse()
+
+	conns := new(grpcconn.Connection)
+	if err := conns.Map(context.Background(), cfg.UpstreamAddr); err != nil {
+		fmt.Printf("failed to establish upstream connections: %v", err)
+		os.Exit(1)
+	}
+	logger := log.New(os.Stdout, level, true)
+	service := service.New(logger, conns)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("failed to announce address", "error", err.Error())
+		os.Exit(1)
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterOrderServer(s, srv)
-	log.Printf("gRPC server listening on port %s", port)
+	pb.RegisterOrderServer(s, service)
+	logger.Info(fmt.Sprintf("gRPC server listening on %s:%d", cfg.Server.Host, cfg.Server.Port))
 	if err := s.Serve(lis); err != nil {
-		log.Fatal(err)
+		logger.Error("failed to serve", "error", err.Error())
+		os.Exit(1)
 	}
 }
