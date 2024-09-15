1770b6c415438bde7bd39537b99f22704aaced5a
kanowfy <nguyendung2002hl@gmail.com>
2024-09-15 15:00:34 +0700

    refactor email service

---
+++ email_service/internal/service/service.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/kanowfy/ecom/email_service/internal/mailer"
+	"github.com/kanowfy/ecom/email_service/pb"
+)
+
+type orderItem struct {
+	Item struct {
+		Product_id string
+		Quantity   uint32
+	}
+	Cost int64
+}
+
+type service struct {
+	logger *slog.Logger
+	mailer mailer.Mailer
+	*pb.UnimplementedEmailServer
+}
+
+func New(logger *slog.Logger, mailer mailer.Mailer) *service {
+	return &service{
+		logger: logger,
+		mailer: mailer,
+	}
+}
+
+func (s *service) SendConfirmation(ctx context.Context, req *pb.SendConfirmationRequest) (*pb.None, error) {
+	s.logger.Info("received a SendConfirmation request")
+
+	var items []*orderItem
+	for _, it := range req.GetOrder().GetItems() {
+		var ordItem orderItem
+		ordItem.Item = struct {
+			Product_id string
+			Quantity   uint32
+		}{Product_id: it.GetItem().ProductId, Quantity: it.GetItem().Quantity}
+		ordItem.Cost = it.GetCost()
+		items = append(items, &ordItem)
+	}
+
+	data := map[string]interface{}{
+		"order_id":             req.GetOrder().OrderId,
+		"shipping_tracking_id": req.GetOrder().ShippingTrackingId,
+		"shipping_cost":        req.GetOrder().ShippingCost,
+		"shipping_address": map[string]interface{}{
+			"street_address": req.GetOrder().ShippingAddress.StreetAddress,
+			"city":           req.GetOrder().ShippingAddress.City,
+			"country":        req.GetOrder().ShippingAddress.Country,
+			"zip_code":       req.GetOrder().ShippingAddress.ZipCode,
+		},
+		"items": items,
+	}
+
+	go func() {
+		err := s.mailer.Send(req.GetEmail(), "confirmation.tmpl", data)
+		if err != nil {
+			s.logger.Error("failed to send email", "error", err.Error())
+		}
+	}()
+
+	return &pb.None{}, nil
+}
--- email_service/main.go
+++ email_service/main.go
@@ -1,52 +1,65 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
+	"log/slog"
 	"net"
+	"os"
 
-	"github.com/kanowfy/ecom/email_service/mailer"
+	"github.com/caarlos0/env/v11"
+	"github.com/kanowfy/ecom/email_service/internal/config"
+	"github.com/kanowfy/ecom/email_service/internal/log"
+	"github.com/kanowfy/ecom/email_service/internal/mailer"
+	"github.com/kanowfy/ecom/email_service/internal/service"
 	"github.com/kanowfy/ecom/email_service/pb"
 
 	"google.golang.org/grpc"
 )
 
-var (
-	port = ":3005"
-)
-
-type mailerConfig struct {
-	host     string
-	port     int
-	username string
-	password string
-	sender   string
-}
-
-type server struct {
-	mailer mailer.Mailer
-	*pb.UnimplementedEmailServer
-}
-
 func main() {
-	cfg := mailerConfig{
-		host:     "smtp.mailtrap.io",
-		port:     2525,
-		username: "ad6c3dc5d1ad98",
-		password: "b45ea57aabed01",
-		sender:   "Ecom <no-reply@ecom.kanowfy.com>",
+	var cfg config.Config
+
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("failed to parse environment variable: %v", err)
+		os.Exit(1)
 	}
-	srv := &server{
-		mailer: mailer.New(cfg.host, cfg.port, cfg.username, cfg.password, cfg.sender),
-	}
-	lis, err := net.Listen("tcp", port)
+
+	flag.StringVar(&cfg.Server.Host, "srv.host", cfg.Server.Host, "server host")
+	flag.IntVar(&cfg.Server.Port, "srv.port", cfg.Server.Port, "server port")
+	flag.StringVar(&cfg.Mail.Host, "mail.host", cfg.Mail.Host, "mail host")
+	flag.IntVar(&cfg.Mail.Port, "mail.port", cfg.Mail.Port, "mail port")
+	flag.StringVar(&cfg.Mail.Username, "mail.username", cfg.Mail.Username, "mail username")
+	flag.StringVar(&cfg.Mail.Password, "mail.password", cfg.Mail.Password, "mail password")
+	flag.StringVar(&cfg.Mail.Sender, "mail.sender", cfg.Mail.Sender, "mail sender")
+
+	var level = slog.LevelDebug
+
+	flag.Func("loglevel", "minimum log level", func(s string) error {
+		if err := level.UnmarshalText([]byte(s)); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	flag.Parse()
+
+	logger := log.New(os.Stdout, level, true)
+	mailer := mailer.New(cfg.Mail.Host, cfg.Mail.Port, cfg.Mail.Username, cfg.Mail.Password, cfg.Mail.Sender)
+	service := service.New(logger, mailer)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port))
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("failed to announce address", "error", err.Error())
+		os.Exit(1)
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterEmailServer(s, srv)
-	log.Printf("gRPC server listening on port %s", port)
+	pb.RegisterEmailServer(s, service)
+	logger.Info(fmt.Sprintf("gRPC server listening on %s:%d", cfg.Server.Host, cfg.Server.Port))
 	if err := s.Serve(lis); err != nil {
-		log.Fatal(err)
+		logger.Error("failed to serve", "error", err.Error())
+		os.Exit(1)
 	}
 }
