a3b64cf9eb2cb6159db3146130979464c3547277
kanowfy <nguyendung2002hl@gmail.com>
2024-09-27 10:28:02 +0700

    instrument frontend svc

--- frontend/internal/handlers/handlers.go
+++ frontend/internal/handlers/handlers.go
@@ -11,9 +11,15 @@ import (
 	"github.com/kanowfy/ecom/frontend/internal/grpcconn"
 	"github.com/kanowfy/ecom/frontend/internal/middleware"
 	"github.com/kanowfy/ecom/frontend/pb"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+const tracerName = "github.com/kanowfy/ecom/frontend/handlers"
+
+var tracer = otel.Tracer(tracerName)
 
 type Handlers struct {
 	logger        *slog.Logger
@@ -37,20 +43,22 @@ func (h *Handlers) renderTemplates(w http.ResponseWriter, filename string, data 
 	}
 	err := ts.Execute(w, data)
 	if err != nil {
-		log.Print(err)
+		h.logger.Error("failed to render template", "error", err)
 	}
 
 }
 
 func (h *Handlers) HomeHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Printf("Session ID: %v", sessionID(r))
+	//log.Printf("Session ID: %v", sessionID(r))
 	h.renderTemplates(w, "home.page.html", nil)
 }
 
 func (h *Handlers) ProductsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	res, err := h.getProducts(r.Context(), "")
+	ctx, span := tracer.Start(r.Context(), "get_products")
+	defer span.End()
+	res, err := h.getProducts(ctx, "")
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to get products", "error", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -59,9 +67,16 @@ func (h *Handlers) ProductsHandler(w http.ResponseWriter, r *http.Request, _ htt
 }
 
 func (h *Handlers) ProductHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	product, err := h.getProduct(r.Context(), ps.ByName("id"))
+	ctx, span := tracer.Start(r.Context(), "get_product")
+	defer span.End()
+
+	productId := ps.ByName("id")
+
+	span.SetAttributes(attribute.String("product_id", productId))
+
+	product, err := h.getProduct(ctx, productId)
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to get product", "error", err)
 		return
 	}
 
@@ -69,17 +84,20 @@ func (h *Handlers) ProductHandler(w http.ResponseWriter, r *http.Request, ps htt
 }
 
 func (h *Handlers) cartDetailsView(w http.ResponseWriter, r *http.Request, page string) {
-	cartItems, err := h.getCart(r.Context(), sessionID(r))
+	ctx, span := tracer.Start(r.Context(), "get_cart")
+	defer span.End()
+
+	cartItems, err := h.getCart(ctx, sessionID(r))
 	if err != nil {
 		if e, ok := status.FromError(err); ok {
 			switch e.Code() {
 			case codes.NotFound:
 				h.renderTemplates(w, "empty_cart.page.html", nil)
 			default:
-				log.Println(e.Code(), e.Message())
+				h.logger.Error("failed to get cart", "error", e.Message(), "error_code", e.Code())
 			}
 		} else {
-			log.Println(err.Error())
+			h.logger.Error("failed to get cart", "error", err)
 		}
 		return
 	}
@@ -94,9 +112,9 @@ func (h *Handlers) cartDetailsView(w http.ResponseWriter, r *http.Request, page 
 	var totalItemPrices int64 = 0
 	var cart_size uint32 = 0
 	for i, item := range cartItems {
-		p, err := h.getProduct(r.Context(), item.GetProductId())
+		p, err := h.getProduct(ctx, item.GetProductId())
 		if err != nil {
-			log.Println(err.Error())
+			h.logger.Error("failed to get product", "error", err)
 			return
 		}
 
@@ -110,9 +128,9 @@ func (h *Handlers) cartDetailsView(w http.ResponseWriter, r *http.Request, page 
 		cart_size += item.Quantity
 	}
 
-	shipCost, err := h.getShipCost(r.Context(), cartItems)
+	shipCost, err := h.getShipCost(ctx, cartItems)
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to get shipping cost", "error", err)
 		return
 	}
 
@@ -132,19 +150,22 @@ func (h *Handlers) ViewCartHandler(w http.ResponseWriter, r *http.Request, _ htt
 }
 
 func (h *Handlers) AddToCartHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	ctx, span := tracer.Start(r.Context(), "add_to_cart")
+	defer span.End()
+
 	r.ParseForm()
 	id := r.FormValue("productid")
 	quantity, _ := strconv.ParseUint(r.FormValue("quantity"), 10, 32)
 
-	p, err := h.getProduct(r.Context(), id)
+	p, err := h.getProduct(ctx, id)
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to get product", "error", err)
 		return
 	}
 
-	err = h.addToCart(r.Context(), sessionID(r), p.GetId(), uint32(quantity))
+	err = h.addToCart(ctx, sessionID(r), p.GetId(), uint32(quantity))
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to add item to cart", "error", err)
 		return
 	}
 
@@ -152,18 +173,21 @@ func (h *Handlers) AddToCartHandler(w http.ResponseWriter, r *http.Request, _ ht
 }
 
 func (h *Handlers) RemoveFromCartHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	ctx, span := tracer.Start(r.Context(), "remove_from_cart")
+	defer span.End()
+
 	r.ParseForm()
 	id := r.FormValue("productid")
 
-	p, err := h.getProduct(r.Context(), id)
+	p, err := h.getProduct(ctx, id)
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to get product", "error", err)
 		return
 	}
 
-	err = h.removeFromCart(r.Context(), sessionID(r), p.GetId())
+	err = h.removeFromCart(ctx, sessionID(r), p.GetId())
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to remove item from cart", "error", err)
 		return
 	}
 
@@ -175,6 +199,9 @@ func (h *Handlers) CheckoutHandler(w http.ResponseWriter, r *http.Request, _ htt
 }
 
 func (h *Handlers) PlaceOrderHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	ctx, span := tracer.Start(r.Context(), "place_order")
+	defer span.End()
+
 	r.ParseForm()
 	var (
 		email       = r.FormValue("email")
@@ -207,9 +234,9 @@ func (h *Handlers) PlaceOrderHandler(w http.ResponseWriter, r *http.Request, _ h
 		},
 	}
 
-	resp, err := h.placeOrder(r.Context(), req)
+	resp, err := h.placeOrder(ctx, req)
 	if err != nil {
-		log.Println(err.Error())
+		h.logger.Error("failed to place order", "error", err)
 		http.Redirect(w, r, "/checkout", http.StatusMovedPermanently)
 		return
 	}
@@ -219,9 +246,9 @@ func (h *Handlers) PlaceOrderHandler(w http.ResponseWriter, r *http.Request, _ h
 		"tracking_id": resp.Order.ShippingTrackingId,
 	})
 
-	err = h.emptyCart(r.Context(), sessionID(r))
+	err = h.emptyCart(ctx, sessionID(r))
 	if err != nil {
-		log.Printf("emptying cart failed: %v", err)
+		h.logger.Error("failed to empty cart", "error", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
